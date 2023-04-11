e438db994a1c5f4014f32b2120ded4f3fa444639
Roberto Giaconia <DeRobyJ@users.noreply.github.com>
2023-04-11 12:57:10 +0200

    tweaks

--- src/agent/player.py
+++ src/agent/player.py
@@ -10,7 +10,8 @@ from pprint import pprint
 logging.config.dictConfig(get_logging_conf("player"))
 logger = logging.getLogger("test")
 
-def away_from_everything(state):
+
+def away_from_everything(state):  # Evasive
     """
     Player A: Away from everything
     This player's intention is to get away from the enemies or from everythin as quick as possible.
@@ -37,7 +38,8 @@ def away_from_everything(state):
     """
     return None
 
-def separate_from_enemies(state):
+
+def separate_from_enemies(state):  # Hiding
     """
     Player B: Separate from enemies
     This player's intention is to use the group of obstacles as separation from the enemies, so that the enemies are forced to circumnavigate the obstacles.
@@ -65,13 +67,14 @@ def separate_from_enemies(state):
     """
     return None
 
-def using_obstacles(state):
+
+def using_obstacles(state):  # Shifty
     """
     Player C: using obstacles to get away
     This player chooses a desired position on the map based on the situation, and then pathfinds towards it
 
     A set of manually selected positions is given to the player to choose from based on the situation.
-    The positions are manually selected like so: 
+    The positions are manually selected like so:
         - Indentify the spaces between close obstacles
         - Put one position at the center of the spaces, and two other positions before and after, to create a tunnel
         - Put other positions at the sides of the map
@@ -94,10 +97,12 @@ def using_obstacles(state):
     """
     return None
 
+
 def anti_gravity(state):
     """
     """
     return None
+
 
 def dqn(state):
     """
@@ -105,6 +110,7 @@ def dqn(state):
     This is a replica of the originally intended test for this environment, applied to our settings.
     """
     return None
+
 
 def dummy(state):
     """
@@ -119,17 +125,18 @@ def dummy(state):
 
 
 STRAT = {
-    "away_from_everything" : away_from_everything,
-    "separate_from_enemies" : separate_from_enemies,
-    "using_obstacles" : using_obstacles,
-    "anti_gravity" : anti_gravity,
-    "dqn" : dqn,
-    "dummy" : dummy,
+    "away_from_everything": away_from_everything,
+    "separate_from_enemies": separate_from_enemies,
+    "using_obstacles": using_obstacles,
+    "anti_gravity": anti_gravity,
+    "dqn": dqn,
+    "dummy": dummy,
 }
+
 
 def get_player_action(state, strategy=None, override=None):
     # TODO: Player strategies
-    if override is not None: # FIXME: Temporary override with random action
+    if override is not None:  # FIXME: Temporary override with random action
         return override
     if strategy is not None:
         return STRAT[strategy](state)
@@ -145,11 +152,12 @@ def player():
 
 @click.command()
 @click.argument("adversary_model")
-@click.option("--visualize","-v", is_flag=True, show_default=True, default=False, help="Visualize")
+@click.option("--visualize", "-v", is_flag=True, show_default=True, default=False, help="Visualize")
 @click.pass_context
 def test(ctx, adversary_model, visualize):
     # adversary_model -> filename
     # Load model and run player strat on that model
     pass
+
 
 player.add_command(test)
